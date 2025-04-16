e9e314cd049827525d222435e64237837387048f
Luiz Gomes <louisnuuk@hotmail.com>
2025-04-16 09:32:40 -0300

    feat

---
+++ src/api-query-objects/index.ts
@@ -0,0 +1,48 @@
+import { ApiEndpoint, createClientNextArchitecture, createServerNextArchitecture } from "@/test";
+import { PatrimonyAndPaymentDataProps, PatrimonyAndPaymentParamProps, PostLoginParamsProps, PostLoginResponse } from "./types";
+
+const API_BASE_URL = 'http://localhost:3000';
+const EXTERNAL_DOG_API_BASE_URL = 'https://dog.ceo/api';
+
+export interface BreedHoundImagesDataProps {
+    message: string[];
+    status: string;
+}
+
+const api = {
+    breed_hound_images: {
+        url: `${EXTERNAL_DOG_API_BASE_URL}/breed/hound/images`,
+        authenticated: false,
+        method: 'get',
+        DATA_PROPS: {} as BreedHoundImagesDataProps,
+    },
+    login: {
+        url: `${API_BASE_URL}/auth/login`,
+        authenticated: false,
+        method: 'post',
+        ARGS_PROPS: {} as PostLoginParamsProps,
+        DATA_PROPS: {} as PostLoginResponse
+    },
+    getPatrimonyAndPaymentsParams: {
+        url: `${API_BASE_URL}/patrimonio-e-pagamento`,
+        authenticated: true,
+        method: 'get',
+        ARGS_PROPS: {} as string,
+        DATA_PROPS: {} as PatrimonyAndPaymentParamProps
+    },
+    getPatrimonyAndPaymentsCalculate: {
+        url: `${API_BASE_URL}/stocks/list`,
+        authenticated: true,
+        method: 'get',
+        ARGS_PROPS: {} as PatrimonyAndPaymentParamProps,
+        DATA_PROPS: {} as PatrimonyAndPaymentDataProps
+    }
+} as const satisfies Record<string, ApiEndpoint>;
+
+const serverQueriesObject = createServerNextArchitecture(api);
+const clientQueriesObject = createClientNextArchitecture(serverQueriesObject, api);
+
+export {
+    serverQueriesObject,
+    clientQueriesObject
+}
---
+++ src/api-query-objects/types.ts
@@ -0,0 +1,56 @@
+export interface PostLoginParamsProps {
+    email: string,
+    password: string,
+}
+
+export interface PostLoginResponse {
+    token: string,
+    id: string,
+}
+
+export interface PatrimonyAndPaymentParamProps {
+    start: string,
+    end: string,
+    monthyContribution: number,
+    symbols: string,
+    monthyContributionIncrementByYear: number,
+    smartContribution: boolean,
+}
+
+export interface PatrimonyAndPaymentDataProps {
+    payments: {
+        year: number,
+        payment: number,
+    }[],
+    quotes: {
+        cumulativeContribution: number,
+        cumulativePayment: number, 
+        date: string, 
+        monthyContribution: number, 
+        patrimony: number, 
+        payment: number,
+        cumulativePosition: number,
+        stocks: {
+            cumulativeContributionWithoutDividends: number,
+            cumulativeContribution: number,
+            cumulativePosition: number, 
+            cumulativePayment: number,
+            ordenedStocks: number,
+            patrimony: number,
+            longName: string,
+            payment: number,
+            quote: number
+            date: string,
+            name: string,
+            url: string,
+        }[]
+    }[],
+    stocks: {
+        ticket: string,
+        url: string,
+    }[],
+    finalStatus: {
+        cumulativePayment: number,
+        patrimony: number
+    }
+}
