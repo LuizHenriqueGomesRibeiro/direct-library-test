9b6edfbd26b25dd8a261a802b312a181086047dd
Luiz Gomes <louisnuuk@hotmail.com>
2025-05-09 12:59:27 -0300

    feat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,6 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const customizedPageProps = {
-    user: { name: 'luiz', id: 'dljkhw813jbfdqw8g2v213'},
-    ...pageProps
-  }
 
-  return <Component {...customizedPageProps} />;
+  return <Component {...pageProps} />;
 }
